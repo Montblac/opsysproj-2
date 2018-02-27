3f1ea64a27a5242bceab8635696945174a0c22e8
Montblac <montblac95@gmail.com>
2018-02-27 00:49:05 -0800

    Implemented some error-checking methods and initialization functions

--- methods.c
+++ methods.c
@@ -1,9 +1,15 @@
 //
 // Created by Sam Leyva on 2/26/18.
 //
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <memory.h>
 
+#define MEMSIZE 524288
+
+// Initialization
 int * createBitMap(){
     return calloc(32, sizeof(int));
 }
@@ -14,4 +20,39 @@ unsigned int * createBitMasks(){
         masks[i] = masks[i+1] << 1;
     }
     return masks;
+}
+int * createPhysicalMem(){
+    return calloc(MEMSIZE, sizeof(int));
+}
+
+// Input-checking
+int isWord(const char * input){
+    if(input == NULL) {
+        return 0;
+    }
+    for (int i = 0; i < strlen(input); ++i) {
+        if (!isalpha(input[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+int isNumber(const char * input){
+    if(input == NULL){
+        return 0;
+    }
+    for(int i = 0; i < strlen(input); ++i){
+        if(!isdigit(input[i])){
+            return 0;
+        }
+    }
+    return 1;
+}
+int isValidName(const char * name){
+    if(name == NULL){return 0;}
+    int size = 0;
+    while(name[size] != NULL){
+        ++size;
+    }
+    return size == 1 ? 1 : 0;
 }
--- methods.h
+++ methods.h
@@ -5,7 +5,14 @@
 #ifndef CS143B_PROJECT2_METHODS_H
 #define CS143B_PROJECT2_METHODS_H
 
+// Initialization
 int * createBitMap();
 unsigned int * createBitMasks();
+int * createPhysicalMem();
+
+// Input-checking;
+int isWord(const char * input);
+int isNumber(const char * input);
+int isValidName(const char * name);
 
 #endif //CS143B_PROJECT2_METHODS_H
